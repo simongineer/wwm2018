72f559b8e7989899c78b14ce66e2f0effa18089a
rahmsimon <rahmsimon@U12WKS12>
2018-01-29 12:16:23 +0100

    Projektdateien hinzufügen.

---
+++ WerWirdMillionär/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WerWirdMillionär
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxGeld = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // groupBoxGeld
+            // 
+            this.groupBoxGeld.BackColor = System.Drawing.Color.Transparent;
+            this.groupBoxGeld.ForeColor = System.Drawing.Color.White;
+            this.groupBoxGeld.Location = new System.Drawing.Point(512, -5);
+            this.groupBoxGeld.Name = "groupBoxGeld";
+            this.groupBoxGeld.Size = new System.Drawing.Size(137, 313);
+            this.groupBoxGeld.TabIndex = 1;
+            this.groupBoxGeld.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(650, 306);
+            this.Controls.Add(this.groupBoxGeld);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Wer Wird Millionär - JZ, KT, SP, CS, SR";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBoxGeld;
+    }
+}
+
---
+++ WerWirdMillionär/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WerWirdMillionär
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ErstellePreisListe();
+        }
+
+        private void ErstellePreisListe()
+        {
+            Label[] labelPreise = new Label[15];
+            Label[] labelZahlen = new Label[15];
+            string[] preise = {"50","100","200","300","500","1.000","2.000","4.000","8.000","16.000","32.000","64.000","125.000","500.000","1 MILLION"};
+            //preise
+            for (int i = 0; i < 15; i++)
+            {
+                labelPreise[i] = new Label();
+                labelPreise[i].Font = new Font("Arial", 12/*, FontStyle.Bold*/);
+                labelPreise[i].ForeColor = Color.Orange;
+                labelPreise[i].Location = new Point(40, 285-i*19);
+                if((i+1)%5==0 && i!=0)//jedes 5te ist weis
+                    labelPreise[i].ForeColor = Color.White;
+
+                labelPreise[i].Text = String.Format("€ {0}", preise[i]);
+                groupBoxGeld.Controls.Add(labelPreise[i]);
+            }
+            
+            //Nummern
+            for(int i = 0; i<15; i++)
+            {
+                labelZahlen[i] = new Label();
+                labelZahlen[i].Font = new Font("Arial", 12/*, FontStyle.Bold*/);
+                labelZahlen[i].ForeColor = Color.Orange;
+                labelZahlen[i].Location = new Point(8, 285 - i * 19);
+                if ((i + 1) % 5 == 0 && i != 0)//jedes 5te ist weis
+                    labelZahlen[i].ForeColor = Color.White;
+                if((i+1)<10)
+                    labelZahlen[i].Text = String.Format("  {0}", i + 1);
+                else
+                    labelZahlen[i].Text = String.Format("{0}", i + 1);
+                groupBoxGeld.Controls.Add(labelZahlen[i]);
+            }
+        }
+    }
+}
