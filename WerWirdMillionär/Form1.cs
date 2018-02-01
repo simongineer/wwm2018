aaa73071bb1f6734f4b39a9fac33bfe96efe4c83
scherzerchristian <scherzerchristian@U12WKS11>
2018-02-01 15:00:06 +0100

    Fertige Gui

--- WerWirdMillionär/Form1.cs
+++ WerWirdMillionär/Form1.cs
@@ -7,44 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.OleDb;
 
 namespace WerWirdMillionär
 {
     public partial class Form1 : Form
     {
-        private Image buttonHover =(Image)Resources.Button_Hover;
+
+        private Image buttonHover = (Image)Resources.Button_Hover;
         private Image buttonIdle = (Image)Resources.Button_Idle;
-        OleDbConnection con = null;
-        private int schwierigkeit;
+
+        private Image jokerHover1 = (Image)Resources.Joker_1_hover;
+        private Image jokerHover2 = (Image)Resources.Joker_2_hover;
+        private Image jokerHover3 = (Image)Resources.Joker_3_hover;
+        private Image joker1Idle = (Image)Resources.Joker_1;
+        private Image joker2Idle = (Image)Resources.Joker_2;
+        private Image joker3Idle = (Image)Resources.Joker_3;
+
         public Form1()
         {
             InitializeComponent();
             ErstellePreisListe();
             ErstelleButtons();
             SetzeTransparenz();
-            verbindeDB();
-        }
-
-        private void verbindeDB()
-        {
-            con = new OleDbConnection();
-            con.ConnectionString = Properties.Settings.Default.DBCon;
-            try
-            {
-                con.Open();
-            }
-            catch(InvalidOperationException ex)
-            {
-                MessageBox.Show("Verbindung mit Datenbank nicht möglich");
-            }
-            startenSpiel();
-        }
-
-        public void startenSpiel()
-        {
-            OleDbCommand cmd = con.CreateCommand();
-            cmd.CommandText = "Select * from tFragen where Schwierigkeit = " + schwierigkeit;
         }
 
         // Made by Ch
@@ -225,6 +209,30 @@ namespace WerWirdMillionär
             pictureBoxMe.BackColor = Color.Transparent;
             pictureBoxMe.BringToFront();
 
+            // button_weiter
+
+            transparentParent = Ui_layer1;
+            me = button_weiter;
+            pictureBoxMe = (PictureBox)me;
+            pos = this.PointToScreen(pictureBoxMe.Location);
+            pos = transparentParent.PointToClient(pos);
+            pictureBoxMe.Parent = transparentParent;
+            pictureBoxMe.Location = pos;
+            pictureBoxMe.BackColor = Color.Transparent;
+            pictureBoxMe.BringToFront();
+
+            // label_question_2
+
+            transparentParent = button_weiter;
+            me = label_weiter;
+            labelMe = (Label)me;
+            pos = this.PointToScreen(labelMe.Location);
+            pos = transparentParent.PointToClient(pos);
+            labelMe.Parent = transparentParent;
+            labelMe.Location = pos;
+            labelMe.BackColor = Color.Transparent;
+            labelMe.BringToFront();
+
 
         }
 
@@ -237,23 +245,23 @@ namespace WerWirdMillionär
         {
             Label[] labelPreise = new Label[15];
             Label[] labelZahlen = new Label[15];
-            string[] preise = {"50","100","200","300","500","1.000","2.000","4.000","8.000","16.000","32.000","64.000","125.000","500.000","1 MILLION"};
+            string[] preise = { "50", "100", "200", "300", "500", "1.000", "2.000", "4.000", "8.000", "16.000", "32.000", "64.000", "125.000", "500.000", "1 MILLION" };
             //preise
             for (int i = 0; i < 15; i++)
             {
                 labelPreise[i] = new Label();
                 labelPreise[i].Font = new Font("Arial", 12/*, FontStyle.Bold*/);
                 labelPreise[i].ForeColor = Color.Orange;
-                labelPreise[i].Location = new Point(40, 285-i*19);
-                if((i+1)%5==0 && i!=0)//jedes 5te ist weis
+                labelPreise[i].Location = new Point(40, 285 - i * 19);
+                if ((i + 1) % 5 == 0 && i != 0)//jedes 5te ist weis
                     labelPreise[i].ForeColor = Color.White;
 
                 labelPreise[i].Text = String.Format("€ {0}", preise[i]);
                 groupBoxGeld.Controls.Add(labelPreise[i]);
             }
-            
+
             //Nummern
-            for(int i = 0; i<15; i++)
+            for (int i = 0; i < 15; i++)
             {
                 labelZahlen[i] = new Label();
                 labelZahlen[i].Font = new Font("Arial", 12/*, FontStyle.Bold*/);
@@ -261,7 +269,7 @@ namespace WerWirdMillionär
                 labelZahlen[i].Location = new Point(8, 285 - i * 19);
                 if ((i + 1) % 5 == 0 && i != 0)//jedes 5te ist weis
                     labelZahlen[i].ForeColor = Color.White;
-                if((i+1)<10)
+                if ((i + 1) < 10)
                     labelZahlen[i].Text = String.Format("  {0}", i + 1);
                 else
                     labelZahlen[i].Text = String.Format("{0}", i + 1);
@@ -277,6 +285,21 @@ namespace WerWirdMillionär
             me.Image = buttonHover;
         }
 
+        private void setJoker1ButtonStateHover(object sender, EventArgs e)
+        {
+            button_joker_1.Image = jokerHover1;
+        }
+
+        private void setJoker2ButtonStateHover(object sender, EventArgs e)
+        {
+            button_joker_2.Image = jokerHover2;
+        }
+
+        private void setJoker3ButtonStateHover(object sender, EventArgs e)
+        {
+            button_joker_3.Image = jokerHover3;
+        }
+
         // Wird beim eintreten in den Ui_layer1 ausgeführt (MouserEnter) (CH)
         private void resetAnswerButtonState(object sender, EventArgs e)
         {
@@ -284,6 +307,10 @@ namespace WerWirdMillionär
             button_answer_2.Image = buttonIdle;
             button_answer_3.Image = buttonIdle;
             button_answer_4.Image = buttonIdle;
+            button_joker_1.Image = joker1Idle;
+            button_joker_2.Image = joker2Idle;
+            button_joker_3.Image = joker3Idle;
+
         }
         // Methode gilt für alle Buttons (MosueClick) (CH)
         private void getAnswerIndex(object sender, MouseEventArgs e)
@@ -293,17 +320,17 @@ namespace WerWirdMillionär
             {
                 String indexSting = ((Control)sender).Name.Split('_')[2];
                 bool result = Int32.TryParse(indexSting, out index);
-                if(result == false)
+                if (result == false)
                 {
                     MessageBox.Show("Something went wrong");
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.StackTrace);
             }
 
-            
+
             MessageBox.Show("Es wurde " + index + " ausgewählt!");
 
 
@@ -328,5 +355,13 @@ namespace WerWirdMillionär
 
             MessageBox.Show("Es wurde Joker " + index + " ausgewählt!");
         }
+
+        private void OnWeiterClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Es wurde auf weiter geklickt");
+            button_weiter.Visible = false;
+            label_weiter.Visible = false;
+        }
+
     }
 }
